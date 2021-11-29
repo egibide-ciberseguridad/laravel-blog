80fd860a00a0cea8bcf670f36282dc9917ff4099
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-11-29 14:37:57 +0100

    Modelo para comentarios

---
+++ database/factories/ComentarioFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ComentarioFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'email' => $this->faker->email(),
+            'texto' => $this->faker->text(100),
+            'fecha' => $this->faker->dateTime(),
+            'visible' => $this->faker->boolean(),
+        ];
+    }
+}
